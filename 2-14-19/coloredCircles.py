e0e5e3087fc758e2b842b357a17b323b9da47158
Akshay Vaswani <34140914+AkshayVaswani@users.noreply.github.com>
2019-03-15 13:05:03 -0400

    coloredcircles

--- 2-14-19/coloredCircles.py
+++ 2-14-19/coloredCircles.py
@@ -9,7 +9,7 @@ alex.color("green")
 alex.pensize(3)
 alex.speed(12)
 
-
+"""
 class Circles:
 
     def circleMake(self, size, color):
@@ -30,7 +30,7 @@ class Circles:
             alex.goto(x, y)
             alex.pendown()
             self.circleMake(si, col)
-            self.xCoor.append(x)
+            self.xCoor[i]
             self.yCoor.append(y)
             self.size.append(si)
     def erase(self):
@@ -41,11 +41,49 @@ class Circles:
             alex.pendown()
             #alex.dot(self.size[i])
             self.circleMake(self.size[i], "white")
-
-s=Circles()
+"""
+#s=Circles()
 #s.circleSpam(random.randint(0, 100))
-s.circleSpam(10)
-s.erase()
+#s.circleSpam(10)
+#s.erase()
+def circlemake(size, color):
+    alex.color(color)
+    alex.dot(size)
+def circlespam(number):
+    xcoor = [None] * number
+    print(len(xcoor))
+    ycoor = [None] * number
+    print(len(ycoor))
+    size = [None] * number
+    print(len(size))
+    for i in range(number):
+        x = random.randint(-250, 250)
+        y = random.randint(-250, 250)
+        n = lambda: random.randint(0, 255)
+        col = ('#%02X%02X%02X' % (n(), n(), n()))
+        si = random.randint(10, 100)
+        alex.penup()
+        alex.goto(x, y)
+        alex.pendown()
+        circlemake(si, col)
+        xcoor[i] = x
+        ycoor[i] = y
+        size[i] = si
+    erase(number, xcoor, ycoor, size)
+
+
+def erase(num, x, y, si):
+    for i in range(num):
+        alex.penup()
+        alex.goto(x[i], y[i])
+        print(str(x[i]) + " " + str(y[i]))
+        alex.pendown()
+        circlemake(si[i], "white")
+
+circlespam(50)
+
+
+
 wn.exitonclick()
 
 
